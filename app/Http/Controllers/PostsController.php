11e5a313e8d99f6a2ac08348c67d2041124f2189
vagrant <vagrant@homestead>
2019-07-24 22:26:39 +0000

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Posts\CreatePostsRequest;
+use App\Http\Requests\Posts\UpdatePostRequest;
+use Illuminate\Support\Facades\Storage;
+use App\Post;
+use App\Category;
+use App\Tag;
+
+
+class PostsController extends Controller
+{
+
+    public function __construct() {
+        $this->middleware('verifyCategoriesCount')->only(['create', 'store']);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index')->with('posts', Post::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create')->with('categories', Category::all())->with('tags', Tag::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreatePostsRequest $request)
+    {
+        //uploads the image
+        $image = $request->image->store('posts');
+        //crea el post
+        $post = Post::create([
+            'title' =>   $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'image' => $image,
+            'category_id' => $request->category,
+            'published_at' => $request->published_at
+        ]);
+        //flash message
+        if ($request->tags) {
+            $post->tags()->attach($request->tags);
+        }
+        session()->flash('success', 'Post created succesfully');
+        //redirect
+        return redirect(route('posts.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.create')->with('post', $post)->with('categories', Category::all())->with('tags', Tag::all());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $data = $request->only(['title', 'description', 'content', 'published_at']);
+        if($request->hasFile('image')) {
+            $image = $request->image->store('posts');
+            $post->deleteImage();
+            $data['image'] = $image;
+        }
+        if ($request->tags) {
+            $post->tags()->sync($request->tags);
+        }
+        $post->update($data);
+        session()->flash('success', 'Post updated succesfully');
+        return redirect(route('posts.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
+        if($post->trashed()) {
+            $post->deleteImage();
+            $post->forceDelete();
+        } else {
+            $post->delete();
+        }
+        session()->flash('success', 'Post deleted succesfully');
+
+        return redirect(route('posts.index'));
+    }
+/**
+     * Displays all trashed posts.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function trashed() {
+        $trashed = Post::onlyTrashed()->get();
+
+        // return view('posts.index')->withPosts($trashed);
+        return view('posts.index')->with('posts', $trashed);
+    }
+
+    public function restore($id) {
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
+        $post->restore();
+        session()->flash('success', 'Post restored succesfully');
+        return redirect()->back();
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use App\Tag;
+use App\Category;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tag1 = Tag::create([
+            'name' => 'Record',
+        ]);
+        $tag2 = Tag::create([
+            'name' => 'Progress',
+        ]);
+        $tag3 = Tag::create([
+            'name' => 'Customers',
+        ]);
+        $tag4 = Tag::create([
+            'name' => 'Record',
+        ]);
+        $category1 = Category::create([
+            'name' => 'Freebie'
+        ]);
+        $category2 = Category::create([
+            'name' => 'Partnership'
+        ]);
+        $category3 = Category::create([
+            'name' => 'Updates'
+        ]);
+        $category4 = Category::create([
+            'name' => 'Design'
+        ]);
+
+        $post1 = Post::create([
+            'title' => 'We relocated our office to a new designed garage',
+            'description' => 'lipsum',
+            'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => $category1->id,
+            'image' => 'posts/1.jpg'
+
+        ]);
+
+        $post2 = Post::create([
+            'title' => 'Top 5 brilliant content marketing strategies',
+            'description' => 'lipsum',
+            'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => $category2->id,
+            'image' => 'posts/2.jpg'
+
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Best practices for minimalist design with example',
+            'description' => 'lipsum',
+            'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => $category3->id,
+            'image' => 'posts/3.jpg'
+
+        ]);
+
+        $post4 = Post::create([
+            'title' => 'Congratulate and thank to Maryam for joining our team',
+            'description' => 'lipsum',
+            'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'category_id' => $category2->id,
+            'image' => 'posts/4.jpg'
+
+        ]);
+
+        $post1->tags()->attach([$tag1->id,$tag2->id]);
+        $post2->tags()->attach([$tag2->id,$tag3->id]);
+        $post3->tags()->attach([$tag3->id,$tag1->id]);
+    }
+}
